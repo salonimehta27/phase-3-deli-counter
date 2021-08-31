3928f8c87478c3ffa6862a19f71dd13aead6051e
Saloni <salonimehta2127@gmail.com>
2021-08-31 15:26:41 -0400

    Completed

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require "pry"
+katz_deli=[]
+  
+def line queue_array 
+ if queue_array.empty?
+    puts "The line is currently empty."
+#   binding.pry
+ else
+    current_line="The line is currently:"
+    queue_array.map.with_index(1) do |name,current_place|
+    current_line<<" #{current_place}. #{name}"
+    end
+  puts current_line
+ end
+end
+
+def take_a_number current_line,new_customer
+ current_line<<new_customer
+ current_line.map.with_index(1) do |current_name,current_place|
+  if current_name==new_customer
+   puts "Welcome, #{current_name}. You are number #{current_place} in line."
+  end
+ end
+end
+
+def now_serving current_line
+    if current_line.empty?
+    puts "There is nobody waiting to be served!"
+    else
+    current_customer=current_line.shift
+    puts "Currently serving #{current_customer}."
+    end
+end
